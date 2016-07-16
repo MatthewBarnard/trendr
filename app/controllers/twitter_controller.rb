57ac8104b355f514dac783a8aa0f9be3505861dc
Matthew Barnard <msbarnard10@gmail.com>
2016-07-16 23:27:33 +0200

    added twitter auth route

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,4 +1,13 @@
 class TwitterController < ApplicationController
+
   def index
+  end
+
+  def authenticate
+    auth_hash = request.env['omniauth.auth']
+    cookies[:twitter_token] = {
+        value: 'token',
+        expires: 1.year.from_now
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :twitter
 
+  get '/auth/twitter/callback', to: 'twitter#authenticate'
+
   controller :twitter do
     get '/twitter' => :index
   end
